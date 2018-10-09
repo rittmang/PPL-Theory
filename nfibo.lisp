918b15faefa650ada45dab6a0e93cc883d9a05ab
Ritom Gupta <ritomgupta99@gmail.com>
2018-10-09 19:13:23 +0530

    Update nfibo.lisp

--- nfibo.lisp
+++ nfibo.lisp
@@ -1,8 +1,8 @@
-(defun nfib(n)
+(defun nfibr(n)
  
  
  (cond ((= n 1)
-			  0
+		  0
 	   )
   
 	   ((= n 2)
@@ -10,11 +10,24 @@
 	   )
 		  
 	   (
-		  (+ (nfib (- n 1)) (nfib (- n 2)))
+		  (+ (nfibr (- n 1)) (nfibr (- n 2)))
 	   )
 		  
  )
-) 
+)
+(defvar a)(defvar b)(defvar c)
+(defun nfibi(x)       
+	     
+	 (setq a 0)(setq b 1)     
+	 (loop for i from 2 to (- x 1)      
+		  do
+		        (setq c (+ a b))
+		           
+		        (setq a b)
+		        (setq b c)    
+	)
+	c
+)
   
 (defvar n)
 (princ "Enter term: ")
@@ -22,6 +35,6 @@
 (setq n (read))
  
 
-(format t "~D term of Fibonacci = ~D~%" n (nfib n))
-
-
+(format t "~D term of Fibonacci = ~D~%" n (nfibi n))
+(format t "~D term of Fibonacci = ~D~%" n (nfibr n))
+;This is considering 0 is 1st term and 1 is 2nd term. In some mathematical definitions, 0 is considered 0th element and 1 is considered 1st element.
